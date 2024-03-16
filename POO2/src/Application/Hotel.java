f15cff937430aefeb9832139acbd0d576be2a145
rachasalhi1 <162183624+rachasalhi1@users.noreply.github.com>
2024-03-16 13:22:33 +0100

    Update Hotel.java

--- POO2/src/Application/Hotel.java
+++ POO2/src/Application/Hotel.java
@@ -7,7 +7,16 @@ public class Hotel {
 		HashMap<Integer,Reservation> h =new HashMap<>();
 		ArrayList<Chambre> h2 =new ArrayList<>();
 		//Afficher les hashmaps
-		
+		System.out.println(" les reservations:");
+	for (Map.Entry<Integer, Reservation> entry : h.entrySet()) {
+	    Integer key = entry.getKey();
+	    Reservation reservation = entry.getValue();
+	    System.out.println("Key: " + key + ", Reservation: " + reservation);
+	}
+	System.out.println("\n les chambres:");
+	for (Chambre chambre : h2) {
+	    System.out.println(chambre);
+	}
 	}
 	
 
