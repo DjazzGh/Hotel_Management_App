634b0bac6773fc7e5c8dc29b76546dac96ca261e
DjazzGh <150776328+DjazzGh@users.noreply.github.com>
2024-03-16 06:29:22 +0100

    Update classe Date.java

--- POO2/src/Application/Date.java
+++ POO2/src/Application/Date.java
@@ -1,5 +1,33 @@
 package Application;
-
+import java.util.*;
 public class Date {
+int jour;
+int mois;
+int année;
 
+public Date() {}
+public Date(int jour,int mois,int année) {
+	this.jour=jour;
+	this.mois=mois;
+	this.année=année;
+}
+public int getjour() {
+	return this.jour;
+}
+public int getmois() {
+	return this.mois;
+}
+public int getannée() {
+	return this.année;
+}
+
+public static int DatetoInt(Date date) {
+	return date.getannée()*360+date.getmois()*30+date.getjour();
+}
+public static boolean avant(Date date1,Date date2) {
+	int a =DatetoInt(date1);
+	int b =DatetoInt(date2);
+	if(a<b) return true;
+	else return false;
+}
 }
