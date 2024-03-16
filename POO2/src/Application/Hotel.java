4d349530fba8b67600c2fce4ef6ba3c35ce6d628
DjazzGh <150776328+DjazzGh@users.noreply.github.com>
2024-03-16 06:33:51 +0100

    Create classe Hotel.java

---
+++ POO2/src/Application/Hotel.java
@@ -0,0 +1,15 @@
+package Application;
+import java.util.*;
+
+
+public class Hotel {
+	public static void main(String [] args) {
+		HashMap<Integer,Reservation> h =new HashMap<>();
+		ArrayList<Chambre> h2 =new ArrayList<>();
+		//Afficher les hashmaps
+		
+	}
+	
+
+	
+}
