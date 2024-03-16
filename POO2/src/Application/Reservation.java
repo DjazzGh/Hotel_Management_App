8c891822a23440dbaf6959de462c354c9aba145f
DjazzGh <150776328+DjazzGh@users.noreply.github.com>
2024-03-16 06:30:49 +0100

    Update classe Reservation.java

--- POO2/src/Application/Reservation.java
+++ POO2/src/Application/Reservation.java
@@ -1,5 +1,12 @@
 package Application;
 
 public class Reservation {
-
+	Date debut;
+	Date fin;
+	Chambre c;
+	public Reservation(Date debut, Date fin,Chambre c) {
+		this.debut=debut;
+		this.fin=fin;
+		this.c=c;
+	}
 }
