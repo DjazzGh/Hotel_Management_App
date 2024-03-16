c2a482a7c8edb3557e0581279572a6b0e368cc05
DjazzGh <150776328+DjazzGh@users.noreply.github.com>
2024-03-16 06:27:19 +0100

    Update classe Chambre.java

--- POO2/src/Application/Chambre.java
+++ POO2/src/Application/Chambre.java
@@ -1,5 +1,22 @@
 package Application;
 
 public class Chambre {
-
+  int numero;
+  Type typechambre;
+  boolean disponibilité;
+  public Chambre(int numero,Type typechambre,boolean disponibilité) {
+	  this.numero=numero;
+	  this.typechambre =typechambre;
+	  this.disponibilité=disponibilité;
+  }
+  
+  public int getnumerochambre() {
+	  return numero;
+  }
+  public Type gettypechambre() {
+	  return typechambre;
+  }
+  public boolean getdisponibiltéchambre() {
+	  return disponibilité;
+  }
 }
