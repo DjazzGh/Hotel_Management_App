ea491ffdfe339f51d7bd708fb77000bbc2a0c085
DjazzGh <150776328+DjazzGh@users.noreply.github.com>
2024-03-16 06:26:36 +0100

    Update classe Administrateur.java

--- POO2/src/Application/Administrateur.java
+++ POO2/src/Application/Administrateur.java
@@ -1,5 +1,23 @@
 package Application;
 
 public class Administrateur extends Utilisateur {
-
+	public static boolean TraiterDemande(Demande d){
+		Chambre c1=d.getchambredemandé();
+		if(c1.getdisponibiltéchambre()==false) {
+			Reservation r = new Reservation(d.getdatedebut(),d.getdatefin(),c1);
+			int c2=d.getIDclient();
+			//Ajouter au Hashmap le client et sa reservation
+			return true;
+		}
+		else {
+		 return false;
+		}
+	}
+	public void AjouterChambre(int numero,Type t,boolean disponibilité) {
+		Chambre c = new Chambre(numero, t ,disponibilité);
+		//Ajouter la chambre a la arraylist
+	}
+	public void SupprimerChambre(Chambre c) {
+		//Supprimer cette chambre de la arraylist
+	}
 }
