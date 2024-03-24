08d23cefde494ed2d05858a90d4b456bf356dc62
DjazzGh <150776328+DjazzGh@users.noreply.github.com>
2024-03-24 10:11:43 +0100

    Modification dans la classe Administrateur

--- POO2/src/Application/Administrateur.java
+++ POO2/src/Application/Administrateur.java
@@ -1,27 +1,29 @@
 package Application;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Administrateur extends Utilisateur {
-	public static boolean TraiterDemande(Demande d){
-		Chambre c1=d.getchambredemandé();
-		if(c1.getdisponibiltéchambre()==false) {
-			Reservation r = new Reservation(d.getdatedebut(),d.getdatefin(),c1);
-			int c2=d.getIDclient();
-			//Ajouter au Hashmap le client et sa reservation
-			Hotel.h.put(c2, r);
-			return true;
-		}
-		else {
-		 return false;
-		}
+	public static boolean TraiterDemande(Demande d) {
+	    int id = d.getIDclient();
+	    Chambre c1 = d.getchambredemandé();
+	    if (c1.getdisponibiltéchambre() == false) {
+	        Reservation r = new Reservation(d.getdatedebut(), d.getdatefin(), c1, id);
+	        List<Reservation> reservations = Hotel.listClientsReservations.get(id);
+	        if (reservations == null) {
+	            reservations = new ArrayList<>();
+	            Hotel.listClientsReservations.put(id, reservations);  
+	        }
+	        reservations.add(r);
+	        return true;
+	    } else {
+	        return false;
+	    }
 	}
-	public void AjouterChambre(int numero,Type t,boolean disponibilité) {
-		Chambre c = new Chambre(numero, t ,disponibilité);
-		//Ajouter la chambre a la arraylist
-		Hotel.h2.add(c);
-		
+
+	public void AjouterChambre(int numero,Type t,boolean disponibilité,double prix) {
+		Chambre c = new Chambre(numero, t ,disponibilité,prix);
+		Hotel.listChambres.add(c);
 	}
 	public void SupprimerChambre(Chambre c) {
-		//Supprimer cette chambre de la arraylist
-		Hotel.h2.remove(c);
+		Hotel.listChambres.remove(c);
 	}
 }
