f119a7224ceba62bee320c13f443d815ca36a217
DjazzGh <150776328+DjazzGh@users.noreply.github.com>
2024-03-24 10:18:36 +0100

    Update classe Facture

--- POO2/src/Application/Facture.java
+++ POO2/src/Application/Facture.java
@@ -1,6 +1,28 @@
 package Application;
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class Facture {
- //Pour chaque client calculer le prix totale de tous ces reservations, 
-	//en utilisant hashmap de Hotel et stocker le resultat dans un Hashmap <Integer(IDClient),Integer(Prix)>
+ 	static HashMap<Integer,Double> hprix=new HashMap<>();
+	public static void main(String [] args) {
+	
+	for (Map.Entry<Integer, List<Reservation>> entry : Hotel.listClientsReservations.entrySet()) {
+	    // Récupérer l'ID du client
+	    int idClient = entry.getKey();
+
+	    // Initialiser le prix total pour le client
+	    double prixTotal = 0.0;
+
+	    // Récupérer la liste des réservations du client
+	    List<Reservation> reservations = entry.getValue();
+
+	    // Parcourir la liste des réservations et ajouter le prix de chaque chambre au prix total
+	    for (Reservation reservation : reservations) {
+	        prixTotal += reservation.getchambre().getprix();
+	    }
+
+	    // Ajouter l'ID du client et le prix total au HashMap des prix
+	    hprix.put(idClient, prixTotal);
+	    
+	}}
 }
