3a45b8cf0a6824d0813c23ab7324554b2ca3987d
DjazzGh <150776328+DjazzGh@users.noreply.github.com>
2024-03-24 10:13:09 +0100

    Ajout de la variable prix dans chambres

--- POO2/src/Application/Chambre.java
+++ POO2/src/Application/Chambre.java
@@ -4,10 +4,12 @@ public class Chambre {
   int numero;
   Type typechambre;
   boolean disponibilité;
-  public Chambre(int numero,Type typechambre,boolean disponibilité) {
+double prix;
+  public Chambre(int numero,Type typechambre,boolean disponibilité,double prix) {
 	  this.numero=numero;
 	  this.typechambre =typechambre;
 	  this.disponibilité=disponibilité;
+	  this.prix=prix;
   }
   
   public int getnumerochambre() {
@@ -18,5 +20,8 @@ public class Chambre {
   }
   public boolean getdisponibiltéchambre() {
 	  return disponibilité;
+  }
+	  public double getprix() {
+	  return this.prix;
   }
 }
