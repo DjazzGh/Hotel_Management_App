b7eebf5b0fdf753f1931622e733283a6afdec463
khenerania <162226485+khenerania@users.noreply.github.com>
2024-03-25 15:28:52 +0100

    Update Reservation.java

--- POO2/src/Application/Reservation.java
+++ POO2/src/Application/Reservation.java
@@ -4,10 +4,12 @@ public class Reservation {
 	Date debut;
 	Date fin;
 	Chambre c;
-	public Reservation(Date debut, Date fin,Chambre c) {
+	int ID;
+	public Reservation(Date debut, Date fin,Chambre c,int ID) {
 		this.debut=debut;
 		this.fin=fin;
 		this.c=c;
+		this.ID=ID;
 	}
 	public Chambre getchambre() {
 		return this.c;
