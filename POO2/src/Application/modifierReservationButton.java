27d4770e8ac3e6ef5fe461cf1500cfa6174c5494
Djazia_Gh <150776328+DjazzGh@users.noreply.github.com>
2024-04-02 16:07:34 +0100

    Create modifierReservationButton.java

---
+++ POO2/src/Application/modifierReservationButton.java
@@ -0,0 +1,17 @@
+package Application;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class modifierReservationButton extends JButton{
+	public modifierReservationButton() {
+		super("Modifier mes réservations");
+		
+		addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent e) {
+	        	
+	        } });
+	}
+}
