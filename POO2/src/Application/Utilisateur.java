200582902623dd8cd8339d0d50e16d80d09dd0c4
Djazia_Gh <150776328+DjazzGh@users.noreply.github.com>
2024-04-02 12:54:56 +0100

    Update Utilisateur.java

--- POO2/src/Application/Utilisateur.java
+++ POO2/src/Application/Utilisateur.java
@@ -18,41 +18,51 @@ public abstract class Utilisateur {
 	 return motdepasse;}
   
  
- public boolean AuthentificationAdmin(String username,String password) {
-	 if(username!=Admin1 && username!=Admin2 && username!= Admin3) {
-		 return false;
-	 }
-	 if(password!=password1 && password!=password2 && password!=password3) {
-		 return false;
-	 }
-	 return true;
-	 
+ public static boolean AuthentificationAdmin(String username,String password) {
+	  if(username.equals(Admin1) && password.equals(password1))
+	  {
+	   return true;
+	  }
+	  else {
+
+	     if(username.equals (Admin2) && password.equals(password2)){
+	      return true ;
+	     }
+	     else {
+
+	       if (username.equals(Admin3) && password.equals(password3) )
+	      {
+	       return true;
+	      }
+	       else {
+
+	       return false ;}
+
+	 }}}
+
+ public static boolean SubscriptionCheck(String Nom, String Prenom, String Email, String NumeroTelephone, String datenaissance, String username, String password) {
+  //Si l'utilisateur n'entre pas une information
+  if(Nom.isEmpty() || Prenom.isBlank() || Email.isBlank() || NumeroTelephone.isBlank() || datenaissance.isBlank() || username.isBlank() || password.isBlank()) {
+   return false;
+  }
+  //Vérification du numéro de telephone
+  if( NumeroTelephone.length()!= 10 || !NumeroTelephone.startsWith("0") )
+  {
+   return false;
+  }
+  Client cl = new Client(Nom,Prenom,Email,NumeroTelephone,datenaissance,username,password);
+  Hotel.listClients.add(cl);
+  return true;
  }
 
- public boolean SubscriptionCheck(String Nom,String Prenom,String Email,String NumeroTelephone,Date datenaissance,String username, String password) {
-	 //Si l'utilisateur n'entre pas une information
-	 if(Nom.isEmpty() || Prenom.isBlank() || Email.isBlank() || NumeroTelephone.isBlank() || Date.isBlank(datenaissance) || username.isBlank() || password.isBlank()) {
-			return false;
-		}
-	 //Vérification du numéro de telephone
-  if( NumeroTelephone.length()!= 10 || !NumeroTelephone.startsWith("0") )
-		{
-			return false;
-		}
-  
- 
-	  Client cl = new Client(Nom,Prenom,Email,NumeroTelephone,datenaissance,username,password);
-	  Hotel.listClients.add(cl);
-	return true; 	
-}
-
- public boolean AuthentificationClient(String username, String password) {
-	 for (Client client : Hotel.listClients) {
-		    if (client.getUsername().equals(username) && client.getPassword().equals(password)) {
-		      return true;
-		    }
-		  }
-		  return false;
-		}
- 
+ public static boolean AuthentificationClient(String username, String password) {
+  for (Client client : Hotel.listClients) {
+	  String nomutilisateur=client.getUsername();
+	  String motdepasse=client.getPassword();
+   if (nomutilisateur.equals(username) && motdepasse.equals(password)) {
+    return true;
+   }
+  }
+  return false;
+ }
 }
