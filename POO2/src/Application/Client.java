299ee83b401309febda8c8b18af4033bd64df3dd
Djazia_Gh <150776328+DjazzGh@users.noreply.github.com>
2024-04-06 18:27:17 +0100

    Update Client.java

--- POO2/src/Application/Client.java
+++ POO2/src/Application/Client.java
@@ -1,8 +1,6 @@
-package Application;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+ package Application;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 public class Client extends Utilisateur {
 	int ID;
@@ -10,101 +8,101 @@ public class Client extends Utilisateur {
 	String Prenom;
 	String Email;
 	String NumeroTelephone;
-	Date datenaissance;
+	Datee datenaissance;
 	static int D=1;//pour le numero sequentiel de ID
 	
-	public Client(String Nom,String Prenom,String Email,String NumeroTelephone,Date datenaissance,int ID )throws Numerotelephone ,ageverification{
-		this.Nom=Nom;
-		this.Prenom=Prenom;
-		this.Email=Email;
-		 this.ID=D;
-                D++;
-		if( NumeroTelephone.length()!= 10 || !NumeroTelephone.startsWith("0") )//si la taille de la chaine est 10 et commence par 0
-		{
-			throw new  Numerotelephone();
-		}
-		this.NumeroTelephone=NumeroTelephone;
-		if (datenaissance.année-Hotel.annee !=18)
-			{
-			throw new ageverification;}
-		this.datenaissance=datenaissance;
-                
-	}
-		public Client(String Nom,String Prenom,String Email,String NumeroTelephone,Date datenaissance,String username,String password){
+	public Client(String Nom,String Prenom,String Email,String NumeroTelephone,Datee datenaissance,String username,String password){
 		this.Nom=Nom;
 		this.Prenom=Prenom;
 		this.Email=Email;
 		this.NumeroTelephone=NumeroTelephone;
 		this.datenaissance=datenaissance;
-		super.NomUtilisateur=username;
-		super.motdepasse=password;
+		this.NomUtilisateur=username;
+		this.motdepasse=password;
 		this.ID=D;
 		D++;
 	}
 	
-	public String DemandeReservation(int ID,Date debut, Date fin,Chambre ch) {
+	/*public Client(String Nom,String Prenom,String Email,String NumeroTelephone,Datee datenaissance,int ID)throws NumeroTelephone{
+		this.Nom=Nom;
+		this.Prenom=Prenom;
+		this.Email=Email;
+		if( NumeroTelephone.length()!= 10 || !NumeroTelephone.startsWith("0") )
+		{
+			throw new  NumeroTelephone();
+		}
+		this.NumeroTelephone=NumeroTelephone;
+		//l faut faire une exception
+		//this.datenaissance=datenaissance;
+		this.ID=D;
+		D++;
+	}*/
+	
+	public static boolean DemandeReservation(int ID,Datee debut, Datee fin,Chambre ch) {
 		Demande d1=new Demande(ID,debut, fin,ch);
 		if(Administrateur.TraiterDemande(d1)==true) {
-			return "Réservation confirmé";
+			return true;			
+		}else {
+			return false;
 		}
-		else { return "Reservation impossible";}
 	}
 	
-	public void ModifierDateReservation(int ID, Date debut, Date fin, Chambre c) {
+	public static void ModifierDateReservation(int ID, Datee debut, Datee fin, Chambre c) {
 	    Demande d2 = new Demande(ID, debut, fin, c);
 	    if (Administrateur.TraiterDemande(d2) == true) {
-	        List<Reservation> reservations = Hotel.listClientsReservations.get(ID);
-	        if (reservations != null) {
-	            for (Reservation r : reservations) {
-	                // Check for matching reservation based on ID (assuming unique identifiers)
-	                if (r.getId() == ID) {  
-	                    r.debut = debut;
-	                    r.fin = fin;
-	                    return;  
-	                }
-	            }
+	    	try {
+	    	int a = Fichier.findLineNumberWithWord("Reservations_Clients", String.valueOf(ID));
+	    	int b = Fichier.findLineNumberWithWord("Reservations_Clients", c.toString());
+	    	if(a==b) {  	
+	    	String line=Fichier.lireLigne(a, "Reservations_Clients");
+	    	String nouvelleReservation= String.valueOf(ID)+" "+debut.toString()+" "+" "+fin.toString()+" "+c.toString();
+	    	Fichier.replaceWordInFile("Reservations_Clients", line,nouvelleReservation );}}
+	    	catch(IOException e) {
+	    		System.out.println("Erreur");
+	    	}
 	        }
-	        System.out.println("Reservation not found for the specified ID.");
-	    } else {
+	     else {
 	        System.out.println("Reservation impossible");
-	    }
-	}
-	public void ModifierChambreReservation(int ID, Date debut, Date fin, Chambre c) {
-	    Demande d3 = new Demande(ID, debut, fin, c);
-	    if (Administrateur.TraiterDemande(d3) == true) {
-	        List<Reservation> reservations = Hotel.listClientsReservations.get(ID);
-	        if (reservations != null) {
-	            for (Reservation r : reservations) {
-	                // Check for matching start and end dates to identify the correct reservation
-	                if (r.debut.equals(debut) && r.fin.equals(fin)) {
-	                    r.c = c;  // Modify the room for the specific reservation
-	                    return;  // No need to continue iterating
-	                }
-	            }
-	        }
-	        System.out.println("Reservation not found for specified dates.");
-	    } else {
-	        System.out.println("Reservation impossible");
-	    }
-	}
-	public void AnnulerReservation(int ID,Reservation r) {
-List<Reservation> reservations = Hotel.listClientsReservations.get(ID); // Get the list of reservations for the given ID
-	    if (reservations != null) {
-	        reservations.remove(r);  // Remove the specific reservation from the list
-	        
-	        // If the list is now empty, remove the entire entry for that ID:
-	        if (reservations.isEmpty()) {
-	            Hotel.listClientsReservations.remove(ID);
-	        }
 	    }
 	}
 
+	
+	public static void ModifierChambreReservation(int ID, Datee debut, Datee fin, Chambre c) {
+	    Demande d3 = new Demande(ID, debut, fin, c);
+	    String reservation_a_changer=String.valueOf(ID)+" "+debut+" "+fin;
+	    if (Administrateur.TraiterDemande(d3) == true) {   
+	    	try {
+	    	String line=Fichier.findLineContainingWord("Reservations_Clients",reservation_a_changer );	    	
+	    	String nouvelleReservation= String.valueOf(ID)+" "+debut.toString()+" "+" "+fin.toString()+" "+c.toString();
+	    	Fichier.replaceWordInFile("Reservations_Clients", line,nouvelleReservation );}
+	    	catch(IOException e) {
+	    		System.out.println("Erreur");
+	    	}
+	        }
+	     else {
+	        System.out.println("Reservation impossible");}}
+	    
+	
+
+	
+	public void AnnulerReservation(Reservation r) {
+		try {
+		Fichier.deleteLineContainingWord("Reservations_Clients", r.toString());
+		r.c.setDisponibilitétrue(r.debut, r.fin);
+		}
+		catch(IOException e) {
+			System.out.println("Erreur");
+		}
+	}
 	public String Feedback() {
 		Scanner sc = new Scanner(System.in);
 		return sc.next();
 		
 	}
-	public String toString() {
-	return ID+" "+Nom+" "+Prenom+" "+Email+" "+NumeroTelephone+" "+datenaissance+" "+NomUtilisateur+" "+motdepasse;
-}
-}
+	 public String getUsername() {
+		return NomUtilisateur;}
+		 public String getPassword() {
+		  return motdepasse;}
+		 public String toString() {
+			 return ID+" "+Nom+" "+Prenom+" "+Email+" "+NumeroTelephone+" "+datenaissance+" "+NomUtilisateur+" "+motdepasse;
+		 }}
