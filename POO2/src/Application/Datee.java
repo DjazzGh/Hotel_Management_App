8f4a26663c745239dc814953eae35176bbdbc86c
Djazia_Gh <150776328+DjazzGh@users.noreply.github.com>
2024-04-07 10:06:33 +0100

    Update Datee.java

--- POO2/src/Application/Datee.java
+++ POO2/src/Application/Datee.java
@@ -1,12 +1,12 @@
 package Application;
 import java.util.*;
-public class Date {
+public class Datee {
 int jour;
 int mois;
 int année;
 
-public Date() {}
-public Date(int jour,int mois,int année) {
+public Datee() {}
+public Datee(int jour,int mois,int année) {
 	this.jour=jour;
 	this.mois=mois;
 	this.année=année;
@@ -38,6 +38,32 @@ public static boolean avant(Date date1,Date date2) {
 }
 		public String toString() {
 			return jour+"/"+mois+"/"+année;
+		}
+		public static Datee toDatee(String date) {
+		    int jour = 0, mois = 0, année = 0;
+		    boolean isJour = true;
+
+		    for (int i = 0; i < date.length(); i++) {
+		        char c = date.charAt(i);
+
+		        if (c == '/') {
+		            if (isJour) {
+		                mois = Integer.parseInt(date.substring(i - 2, i));
+		                isJour = false;
+		            } else {
+		                année = Integer.parseInt(date.substring(i - 4, i));
+		                break;
+		            }
+		        } else if (Character.isDigit(c)) {
+		            if (isJour) {
+		                jour = jour * 10 + c - '0';
+		            } else {
+		                mois = mois * 10 + c - '0';
+		            }
+		        }
+		    }
+
+		    return new Datee(jour, mois, année);
 		}
 
 }
