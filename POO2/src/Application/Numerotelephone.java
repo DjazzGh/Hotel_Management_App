b74deef1f1beaebbe3a02508db0695db7ff91691
Djazia_Gh <150776328+DjazzGh@users.noreply.github.com>
2024-05-03 19:20:09 +0100

    Delete POO2/src/Application/Numerotelephone.java

--- POO2/src/Application/Numerotelephone.java
+++
@@ -1,7 +0,0 @@
-package Application;
-public  class Numerotelephone extends Exception {
-  private String msg="le numero de telephone est invalide !! ";
-  public String getmsg()
-    {
-    return msg;}
-  
