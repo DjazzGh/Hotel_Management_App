6bc9e2368b54c2e2df1adba8cf4022532a2a924d
Djazia_Gh <150776328+DjazzGh@users.noreply.github.com>
2024-05-03 17:46:50 +0100

    Update Administrateur.java

--- POO2/src/Application/Administrateur.java
+++ POO2/src/Application/Administrateur.java
@@ -3,34 +3,45 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-public class Administrateur extends Utilisateur {
+public class Administrateur extends Utilisateur{
+
+	
 	public static boolean TraiterDemande(Demande d) {
 	    int id = d.getIDclient();
 	    Datee debut = d.getdatedebut();
     	Datee fin = d.getdatefin();
 	    Chambre c1 = d.getchambredemandé();
-	    if (c1.getdisponibiltéchambre(debut,fin) == true) {  
-	    	c1.setDisponibilitéfalse(debut,fin);
-	    	Reservation r= new Reservation(id,debut,fin,c1);			
-			Fichier.addToFile("Reservations_Clients", r.toString());
+	    String lachambre=Fichier.findLinesWithPrefixCombined("Chambres", String.valueOf(c1.numero));
+	    boolean b=Fichier.CheckAvailabilityofRoom(lachambre,debut.toString(), fin.toString());
+	    if (b == true) {  
+
 	        return true;
 	    } else {
 	        return false;
 	    }
 	}
 
+	
 	public static void AjouterChambre(Type t,double prix) {
+		Chambre c = new Chambre(t,prix);
+		 Fichier.addToFile("Chambres",c.toString1());
+
+	}
+	public static Chambre AjouterChambre1(Type t,double prix) {
 		Chambre c = new Chambre(t ,prix);
-		 Fichier.addToFile("Chambres",c.toString());
+		 return c;
+
 	}
-	}
+	
+	
 	public static void SupprimerChambre(int numero) {
 		try {
 			Fichier.deleteLineContainingWord("Chambres", String.valueOf(numero));
 		}catch(IOException exe) {
 			System.out.println("Erreur");
-		}	
+		}		
 		
 	}
-	
+
 }
+
